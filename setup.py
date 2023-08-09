22f4a6d040ae52f3901003b62abd68d8b33484f7
hjem <hanene.jemoui@ucd.ie>
2023-08-09 17:50:36 +0200

    web app structure added

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    description="its a pricing package",
+    description="its a guess package",
     author="hjem",
     packages=find_packages(),
     license="To define"
