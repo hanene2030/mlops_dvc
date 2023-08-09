d6a0a4c2ef652fa40269de50e5095655895bd59a
hjem <hanene.jemoui@ucd.ie>
2023-08-09 16:03:04 +0200

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a pricing package", 
+    author="hjem", 
+    packages=find_packages(),
+    license="To define"
+)
